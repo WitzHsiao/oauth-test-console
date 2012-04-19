981174bb0d2ac51f92bf3c07c90cedfcdff55c49
Witz Hsiao <witz.hsiao@gmail.com>
2012-04-19 17:27:03 +0800

    1. add zendframework into codeIgniter 2. use zend Oauth client to grant accesstoken

--- application/views/test_console.php
+++ application/views/test_console.php
@@ -3,6 +3,7 @@
     <head>
         <title></title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <script src="http://code.jquery.com/jquery-1.7.2.min.js" type="text/javascript"></script>
         <style type="text/css">
             .input_width {
                 width: 400px;
@@ -34,9 +35,9 @@
             <input type="text" id="ts" name="ts" class="input_width" />
         </p>
         <p>
-            <button id="btn_get_request_token">Get Request Token</button>
-            <button id="btn_open_authorization_url">Open Authorization URL</button>
-            <button id="btn_get_acesss_token">Get Access Token</button>
+            <button id="btn_get_request_token" onclick="get_request_token()">Get Request Token</button>
+            <button id="btn_open_authorization_url" onclick="open_authorization_url()">Open Authorization URL</button>
+            <button id="btn_get_acesss_token" onclick="get_access_token()">Get Access Token</button>
         </p>
         <p>
             <label>api: </label>
@@ -51,5 +52,34 @@
             <label>Response: </label>
             <textarea class="readonly_textarea" id="response" readonly></textarea>
         </p>
+        
+        <script  type="text/javascript">
+            
+            function get_request_token() {
+                var consumerKey = $('#ck').val();
+                var consumerSecret = $('#cs').val();
+                var url = 'http://localhost:8888/CI_OauthTestConsole/index.php/oauth/request_token';
+                
+                $.post(url, 
+                    { ck: consumerKey, cs: consumerSecret}, 
+                    function(data){
+                        $('#tk').val(data.oauth_token);
+                        $('#ts').val(data.oauth_secret);
+                    }, 
+                    'json'
+                );
+            }
+            
+            function open_authorization_url() {
+                var url = 'http://hello.local:8888/oauth/authorize?oauth_token=' + $('#tk').val();
+                window.open(url);
+            }
+            
+            function get_access_token() {
+                var url = 'http://localhost:8888/CI_OauthTestConsole/index.php/oauth/access_token?request_token=' + 
+                    $('#tk').val() + '&ck=' + $('#ck').val() + '&cs=' + $('#cs').val();
+                window.open(url);
+            }
+        </script>
     </body>
 </html>
